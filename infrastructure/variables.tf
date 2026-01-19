fcf6f45b6646a81804756bd1071b9af92b213056
MohamedIKenedy <mohamedifqir99@gmail.com>
2026-01-19 21:29:10 +0100

    Upgrade to t3.medium for sufficient Docker storage

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -10,7 +10,7 @@ variable "name" {
 
 variable "instance_type" {
   type    = string
-  default = "t3.small"  # Increased from t3.micro for more storage (16GB vs 8GB)
+  default = "t3.medium"  # Increased from t3.small for much more storage (40GB vs 16GB)
 }
 
 variable "key_name" {
